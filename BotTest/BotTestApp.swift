e32d2736870931054d752ebc8e60a787429c9ad2
Ray Hwang <ray.hwang@originalfunction.com>
2020-10-20 00:41:55 -0400

    Initial Commit

---
+++ BotTest/BotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotTestApp.swift
+//  BotTest
+//
+//  Created by Ray Hwang on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct BotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
